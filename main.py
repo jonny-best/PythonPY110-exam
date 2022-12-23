c3a92097e5db4eef208e1de08ca0d207f490860f
Evgeny Petrov <jonny-best@mail.ru>
2022-12-23 09:57:17 +0300

    пробую собрать генератор

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from conf import MODEL
 BOOKS_FILE = "books.txt"
 pk = 1
 
+
 def get_title():
     with open(BOOKS_FILE, encoding="utf-8") as f:
         data = f.readlines()
@@ -28,24 +29,34 @@ def get_price():
 
 def get_author():
     fake = Faker()
-    return fake.name()
+    list_author = []
+    for i in range(random.randint(1, 3)):
+        list_author.append(fake.name())
+    return list_author
+
 
 def count(start_number: float = 1, step: float = 1):
     while True:
-        yield start_number
+        fields = {
+            "title": get_title(),
+            "year": get_year(),
+            "pages": get_pages(),
+            "isbn13": get_isbn13(),
+            "rating": get_rating(),
+            "price": get_price(),
+            "author": get_author()
+        }
+
+        yield fields
         start_number += step
 
 if __name__ == '__main__':
-    fields = {
-        "title": get_title(),
-        "year": get_year(),
-        "pages": get_pages(),
-        "isbn13": get_isbn13,
-        "rating": get_rating,
-        "price": get_price,
-        "author": get_author
-    }
+    my_count = count()
+    for _ in range(10):
+        next(my_count)
+        print(next(my_count))
+        pk += 1
 
-print(fields)
+
 
 
