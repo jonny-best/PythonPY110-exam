db450ad78325de2cc83b77868e642581b6ccefdc
Evgeny Petrov <jonny-best@mail.ru>
2022-12-25 13:01:47 +0300

    дописал код и сделал аннотацию

--- main.py
+++ main.py
@@ -2,42 +2,49 @@ import random
 from faker import Faker
 from conf import MODEL
 BOOKS_FILE = "books.txt"
-pk = 1
 
-
-def get_title():
+def get_title() -> str: #Генерирем название книги случайным образом
     with open(BOOKS_FILE, encoding="utf-8") as f:
         data = f.readlines()
     return random.choice(data)
 
-def get_year():
+def get_year() -> int: #Генерируем год случайным образом
     return random.randint(1980, 2022)
 
-def get_pages():
+def get_pages() -> int: #Генерируем номер страницы случайным образом
     return random.randint(1, 462)
 
-def get_isbn13():
+def get_isbn13() -> str: #Генерируем книжный номер случайным образом
     fake = Faker()
     return fake.isbn13()
 
-def get_rating():
+def get_rating() -> int: #Генерируем число с плавающей запятой в диапазоне от 0 до 5 обе границы включительно случайным образом
     return round(random.random(), 5)
 
-def get_price():
+def get_price() -> int: #Генерируем число с плавающей запятой случайным образом
     i = random.randint(1, 5)
     return round(random.random(), i)
 
-def get_author():
+def get_author() -> list: #Генерируем список авторов, который одержит от 1 до 3 авторов. Имя и фамилия автора выбираются случайным образом с помощью модуля Faker
     fake = Faker()
     list_author = []
     for i in range(random.randint(1, 3)):
         list_author.append(fake.name())
     return list_author
 
-
-def count(start_number: float = 1, step: float = 1):
+def count(start_number: float = 1, step: float = 1) -> dict:
+    """
+    Создём функцию-генератор, которая возвращает словарь
+    :param start_number:
+    :param step:
+    :yield:
+    """
+    pk = start_number #стартовая позиция автоинкремента
     while True:
-        fields = {
+        dict_ = {
+            "model": MODEL,
+                    "pk": pk,
+                           "fields": {
             "title": get_title(),
             "year": get_year(),
             "pages": get_pages(),
@@ -46,16 +53,14 @@ def count(start_number: float = 1, step: float = 1):
             "price": get_price(),
             "author": get_author()
         }
+        }
+        yield dict_
+        pk += step #увеличиваем показатель счётчика на 1
 
-        yield fields
-        start_number += step
+if __name__ == '__main__': #Запуск программы
 
-if __name__ == '__main__':
     my_count = count()
-    for _ in range(10):
-        next(my_count)
-        print(next(my_count))
-        pk += 1
+    print(next(my_count))
 
 
 
